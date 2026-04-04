2370aa4b269a464071fde37a799cc0d71495bb97
Md Danish Ali <alidanish373@gmail.com>
2026-04-04 13:22:21 +0530

    Initialize React+Vite with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-center text-3xl font-black underline'>North Roadways Service</h1>
+    </div>
+  )
+}
+
+export default App
